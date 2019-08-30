e0048b8a26680776a04fe981e017c1be00f1f430
leandroscalercio <54578439+leandroscalercio@users.noreply.github.com>
2019-08-30 16:25:20 -0300

    projeto base

---
+++ calclog/calclog/BooleanOperators.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calclog
+{
+    public class BooleanOperators
+    {
+        public bool negacao(bool p) => !p;
+        public bool conjuncao(bool p, bool q) => p & q;
+        public bool disjuncao(bool p, bool q) => p | q;
+        public bool disjuncao_exclusiva(bool p, bool q) => p ^ q;
+        public bool bicondicional(bool p, bool q) => negacao(disjuncao_exclusiva(p, q));
+        public bool condicional(bool p, bool q) => negacao(conjuncao(p,negacao(q)));
+    }
+}
---
+++ calclog/calclog/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace calclog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(252, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
